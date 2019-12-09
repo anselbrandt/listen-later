19e8ec9d74ecb4276dc92ce679000b454ce01457
ansel brandt <ansel@anselbrandt.com>
2019-12-09 17:59:20 -0500

    Add test endpoint

---
+++ src/lambda/test.js
@@ -0,0 +1,36 @@
+import ytpl from 'ytpl';
+import ytdl from 'ytdl-core';
+
+export async function handler(event, context) {
+  try {
+    const id = 'PLNfjgyhQqUxabn9GB83ZfHpOULCK9Zlpi';
+    const playlist = await ytpl(id);
+    const urls = playlist.items.map(item => {
+      return item.url_simple;
+    });
+    const descriptions = await Promise.all(
+      urls.map(async url => {
+        const info = await ytdl.getInfo(url);
+        const format = ytdl.chooseFormat(info.formats, { quality: 140 });
+        const item = `${info.title}`;
+        return item;
+      }),
+    );
+    const results = `
+    ${playlist ? playlist.title : 'Playlist fetch failed'}
+    ${descriptions.join('\n    ')}`;
+    const contentType = 'text';
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': contentType,
+      },
+      body: results,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
+    };
+  }
+}
