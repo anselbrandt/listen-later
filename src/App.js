f14d432a4bb091186349613e9db370b6ef6dc530
ansel brandt <ansel@anselbrandt.com>
2019-12-09 03:40:42 -0500

    Update splash page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <a href="https://listenlater.netlify.com">Get started!</a>
     </div>
   );
 }
